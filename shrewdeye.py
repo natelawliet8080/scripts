8008ca728b69c7351ee2672857f1d8493e758c73
Mahmoud Eldeeb <ohmycactus@protonmail.com>
2025-01-28 22:41:28 +0200

    Update shrewdeye.py

--- shrewdeye.py
+++ shrewdeye.py
@@ -11,7 +11,6 @@ def print_message():
     print("\n")
     print(f"{MAGENTA}Subdomains collector from shrewdeye.app. Just provide a file with all the domains.{RESET}")
     print("\n")
-
 def download_file(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
@@ -20,16 +19,13 @@ def download_file(url, filename):
         print(f"Downloaded: {filename}")
     else:
         print(f"Failed to download {filename}. Status code: {response.status_code}")
-
 def main(domains_file, separate_files, output_file):
     if separate_files:
         output_directory = "shrewdeye_subdomains"
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
-
     with open(domains_file, 'r') as file:
         domains = [line.strip() for line in file.readlines()]
-
     if separate_files:
         for domain in domains:
             if domain:
@@ -48,10 +44,8 @@ def main(domains_file, separate_files, output_file):
                     else:
                         print(f"Failed to download {domain}.txt. Status code: {response.status_code}")
         print(f"All subdomains compiled into: {output_file}")
-
 if __name__ == "__main__":
     print_message()
-
     parser = argparse.ArgumentParser(description="Subdomains collector from shrewdeye.app")
     parser.add_argument("domains_file", help="File containing the list of domains")
     parser.add_argument("-s", "--separate", action="store_true", help="Save each domain in a separate file")
